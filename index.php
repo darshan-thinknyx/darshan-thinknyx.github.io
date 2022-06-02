cfcbe74461e2c1a81461610842307dc77bd087b5
darshan-thinknyx <93512322+darshan-thinknyx@users.noreply.github.com>
2022-06-02 21:50:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <body>
 
 <h1>Test Playground</h1>
-<iframe width="850" height="680" src="ec2-35-88-252-226.us-west-2.compute.amazonaws.com:7681/" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe width="850" height="680" src="52.24.138.49:7681/" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 <?php
 echo "Hello World!";
 ?>
