1f1ce7f72c04ff35cba3b2f450e697d64cdb2697
darshan-thinknyx <93512322+darshan-thinknyx@users.noreply.github.com>
2022-06-02 18:42:49 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Test Playground</h1>
+<iframe width="850" height="680" src="ec2-35-88-252-226.us-west-2.compute.amazonaws.com:7681/" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<?php
+echo "Hello World!";
+?>
+
+</body>
+</html>
